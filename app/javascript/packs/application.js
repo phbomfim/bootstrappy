7afdfda6112a5a3c36cebb16afae35c1802dcf3c
PauloBommfim <paulobommfim@gmail.com>
2019-09-20 12:24:36 -0300

    configuration to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,11 @@
 // const imagePath = (name) => images(name, true)
 
 console.log('Hello World from Webpacker')
+
+import "bootstrap"
+import "../stylesheets/application"
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
